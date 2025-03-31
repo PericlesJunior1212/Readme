07b7b888ac38b0b1bdea024145d3095b19c3fd17
PericlesJunior1212 <periclesjunior2022@gmail.com>
2025-03-31 20:59:14 -0300

    Update Servicos.tsx

--- Servicos.tsx
+++ Servicos.tsx
@@ -1,15 +1,17 @@
+
+
 const servicos = [
-    {id_servicos:1, nome_servico: "Corte normal" },
-    {id_servicos:2, nome_servico: "Cílios" },
-    {id_servicos:3, nome_servico: "Sonbracelhas" },
-    {id_servicos:4, nome_servico: "Corte degraDê" },
-    {id_servicos:5, nome_servico: "Corte militar" },
-    {id_servicos:6, nome_servico: "Alisamento" },
-    {id_servicos:7, nome_servico: "Corte normal" },
-    {id_servicos:8, nome_servico: "Pivete loiro" },
-    {id_servicos:9, nome_servico: "Pigmentação" },
-    {id_servicos:10, nome_servico: "Corte normal" },
-
+   
+           {id_servicos: 1, nome_servicos: "barbeiro"},
+           {id_servicos: 2, nome_servicos: "cabeleleiro"},
+           {id_servicos: 3, nome_servicos: "manicure"},
+           {id_servicos: 4, nome_servicos: "cavagem cabelo"},
+           {id_servicos: 5, nome_servicos: "cabeleleiro"},
 ]
-
-
+        
+           
+           
+           
+           export const Servicos = function (){
+            return servicos;
+         }
