50d0b20f1050e009f40585842f6f41756cf92310
PericlesJunior1212 <periclesjunior2022@gmail.com>
2025-03-31 21:18:23 -0300

    Add files via upload

--- Agendamentos.tsx
+++ Agendamentos.tsx
@@ -1,18 +1,55 @@
-const agendamentos = [
+import { Image, StyleSheet, Platform, Text, FlatList, View, Pressable } from 'react-native';
+import { Agendamentos } from '@/constants/agendamentos';
+import { Link } from 'expo-router';
 
-    {id_agendamentos:1, data:"26/08/11" , hora:"12:30", nome_cliente:"Marta Souza", nome_profissional:"Fulano" },
-    {id_agendamentos:2, data:"15/02/22" , hora:"15:00", nome_cliente:"Gean Silva", nome_profissional:"Fulano" },
-    {id_agendamentos:3, data:"12/09/25" , hora:"14:00", nome_cliente:"Amanda Batista", nome_profissional:"Fulano" },
-    {id_agendamentos:4, data:"26/12/98" , hora:"13:00", nome_cliente:"Juninho Pernabucano", nome_profissional:"Fulano" },
-    {id_agendamentos:5, data:"08/06/25" , hora:"11:30", nome_cliente:"Léo Jardimn", nome_profissional:"Fulano" },
-    {id_agendamentos:6, data:"07/01/24" , hora:"10:00", nome_cliente:"Thainara Costa", nome_profissional:"Fulano" },
-    {id_agendamentos:7, data:"03/10/25" , hora:"17:00", nome_cliente:"Carla Perez", nome_profissional:"Fulano" },
-    {id_agendamentos:7, data:"01/06/25" , hora:"15:00", nome_cliente:"Rosimeire LIma", nome_profissional:"Fulano" },
-    {id_agendamentos:8, data:"01/05/25" , hora:"16:00", nome_cliente:"Carlos", nome_profissional:"Fulano" },
-    {id_agendamentos:9, data:"01/05/25" , hora:"19:00", nome_cliente:"Jessica", nome_profissional:"Fualno" },
-    {id_agendamentos:10, data:"01/05/25" , hora:"12:00", nome_cliente:"Caio", nome_profissional:"Fulano" },
- 
- ]
- export const Agendamnetos = function (){
-             return agendamentos;
-          }
+
+
+export default function HomeScreen() {
+  return (
+    <View style={{}}>
+      
+      
+      <Text style={styles.titleContainer}>Perfil de Clientes</Text>
+      
+      <FlatList
+      data={Agendamentos()}
+      keyExtractor={(item) => item.id_agendamentos.toString()}
+      renderItem={({item}) => (
+        <View style={{}}>
+          <Text>Id: {item.id_agendamentos}</Text>
+          <Text>Data: {item.data}</Text>
+          <Text>Hora: {item.hora}</Text>
+          <Text>Nome do Cliente: {item.nome_cliente}</Text>
+          <Text>Nome do Profissional: {item.nome_profissional}</Text>
+
+
+        </View>
+
+      )}
+      />
+
+    </View>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
+
+
